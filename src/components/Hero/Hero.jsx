8003a441afacd7f4b4bef4cbbfd5b48956decfa1
Asad9340 <asadulimran1999@gmail.com>
2024-03-22 15:58:50 +0600

    added banner modification

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,37 +4,33 @@ function Hero() {
       <section className="bg-gray-800 text-gray-100">
         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
           <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-              Ac mattis
-              <span className="text-violet-400">senectus</span>erat pharetra
+            <h1 className="text-4xl font-bold leading-none mb-5 ">
+              Style Haven: Where <br />
+              Beauty
+              <span className="text-violet-400"> Blossoms</span>
             </h1>
-            <p className="mt-6 mb-8 text-lg sm:mb-12">
-              Dictum aliquam porta in condimentum ac integer
-              <br className="hidden md:inline lg:hidden" />
-              turpis pulvinar, est scelerisque ligula sem
-            </p>
             <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
               <a
                 rel="noopener noreferrer"
                 href="#"
                 className="px-8 py-3 text-lg font-semibold rounded bg-violet-400 text-gray-900"
               >
-                Suspendisse
+                Beauty Style
               </a>
               <a
                 rel="noopener noreferrer"
                 href="#"
                 className="px-8 py-3 text-lg font-semibold border rounded border-gray-100"
               >
-                Malesuada
+                Hair Cut
               </a>
             </div>
           </div>
           <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
             <img
-              src="assets/svg/Business_SVG.svg"
+              src="https://images.unsplash.com/photo-1634449571010-02389ed0f9b0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
               alt=""
-              className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+              className="object-contain w-96 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128 rounded-xl"
             />
           </div>
         </div>
@@ -43,4 +39,4 @@ function Hero() {
   );
 }
 
-export default Hero
+export default Hero;
